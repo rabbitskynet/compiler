c2bfb986f0a3bdd457c167360f81cd2e5932a54e
Jack Smith <rabbitskynet@gmail.com>
2015-12-02 16:08:06 +0300

    completed lab2

--- calc.py
+++ calc.py
@@ -16,14 +16,11 @@ tokens = [
 class Exp: pass
 
 class Node(Exp):
-	def __init__(self, tokenname, value1 = None, operand = None, value2 = None, value3 = None, value4 = None):
+	def __init__(self, tokenname, operand = None, array = None):
 		self.name = random.getrandbits(64)
 		self.token = tokenname
-		self.left = value1
-		self.right = value2
-		self.third = value3
-		self.four = value4
 		self.op = operand
+		self.array = array
 
 t_LPAREN	= r'\('
 t_RPAREN	= r'\)'
@@ -38,9 +35,13 @@ def t_COMMENT(t):
 	pass
 
 def t_IDENTIFIER(t):
-	r'[a-zA-Z_][a-zA-Z0-9_]*'
-	if re.match(r'[VIX][VIX]*',t.value):
-		t.type = reserved.get(t.value.lower(),'ROMAN')
+	r'[0a-zA-Z_][a-zA-Z0-9_]*'
+	if re.match(r'^[MDCLXVI]+$',t.value):
+
+		t.type = 'ROMAN'
+	elif re.match(r'0',t.value):
+		t.type = 'ROMAN'
+		t.value= 'nulla'
 	else:
 		t.type = reserved.get(t.value.lower(),'IDENTIFIER')
 	return t
@@ -59,21 +60,8 @@ def t_error(t):
 # Build the lexer
 import ply.lex as lex
 lex.lex()
-lines = '\n'.join([line.rstrip('\n') for line in open('code.txt')])
+lines = ' '.join([line.rstrip('\n') for line in open('code.txt_old')])
 print lines
-#data= "VIX + XX for int"
-#data=lines
-#lexer.input(data)
-
-#exit()
-# Tokenize
-"""while True:
-    tok = lexer.token()
-    if not tok:
-        break      # No more input
-    print(tok)
-"""
-#exit()
 
 
 # Parsing rules
@@ -88,34 +76,54 @@ precedence = (
 
 def p_expression_less(p):
 	'expression : expression INEQUALITY expression'
-	p[0] = Node('INEQUALITY-expression',p[1],p[2],p[3])
+	p[0] = Node('INEQUALITY-expression',p[2], [p[1],p[3]])
+
+def p_expression_multy(p):
+	'expression : expression SEPARATOR expression'
+	if isinstance(p[1],Node):
+		if p[1].token == 'ORDER-expression':
+			p[1].array.append(p[3])
+			p[0] = p[1]
+		elif p[3].token == 'ORDER-expression':
+			p[3].array.insert(0, p[1])
+			p[0] = p[3]
+		else:
+			p[0] = Node('ORDER-expression',p[2],[p[1],p[3]])
+	else:
+		p[0] = Node('ORDER-expression',p[2],[p[1],p[3]])
+
+def p_expression_endexp(p):
+	'expression : expression SEPARATOR'
+	p[0] = p[1]
+
 
 def p_expression_binop(p):
 	'''expression : expression '+' expression
 				  | expression '-' expression
 				  | expression '*' expression
 				  | expression '/' expression'''
-	p[0] = Node('binary-expression',p[1],p[2],p[3])
+	p[0] = Node('binary-expression',p[2],[p[1],p[3]])
 
 def p_expression_for(p):
 	'expression : FOR LPAREN expression SEPARATOR expression SEPARATOR expression RPAREN DO expression DONE'
-	p[0] = Node('for-expression',p[3],'for',p[5],p[7],p[10])
+	p[0] = Node('for-expression','for',[p[3],p[5],p[7],p[10]])
 
 def p_expression_group(p):
 	'expression : LPAREN expression RPAREN'
-	p[0] = Node('group-expression',p[2])
+	#p[0] = Node('group-expression','()', [p[2]])
+	p[0] = p[2]
 
 def p_expression_assign(p):
 	'expression : expression ASSIGN expression'
-	p[0] = Node('assign-expression',p[1],p[2],p[3])
+	p[0] = Node('assign-expression',p[2],[p[1],p[3]])
 
 def p_expression_roman(p):
 	'expression : ROMAN'
-	p[0] = Node('roman-expression',p[1])
+	p[0] = Node('roman-expression','XX', [p[1]])
 
 def p_expression_id(p):
 	'expression : IDENTIFIER'
-	p[0] = Node('id-expression',p[1])
+	p[0] = Node('id-expression','id',[p[1]])
 
 def p_error(p):
 	if p:
@@ -126,61 +134,53 @@ def p_error(p):
 import ply.yacc as yacc
 yacc.yacc()
 
-#exit()
+
+
 
 import subprocess
 
 res = yacc.parse(lines)
 
-print res.token
+def WriteParentLine(fileH, parent, onenode, orderid = None):
+	if parent != "start":
+		if orderid:
+			fileH.write("\t{} -> {} [label=\"{}\"]\n".format(parent, onenode.name, orderid))
+		else:
+			fileH.write("\t{} -> {}\n".format(parent, onenode.name))
 
-
-def getItem(fileH, parent, array):
-	print array.token
-	if array.token == 'for-expression':
-		fileH.write("\t{} [label=\"FOR\"]\n".format(array.name))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-		getItem(fileH, array.name, array.left)
-		getItem(fileH, array.name, array.right)
-		getItem(fileH, array.name, array.third)
-		getItem(fileH, array.name, array.four)
-	elif array.token == 'binary-expression':
-		fileH.write("\t{} [label=\"Bin:{}\"]\n".format(array.name,array.op))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-		#~ print array.left,array.right
-		getItem(fileH, array.name, array.left)
-		getItem(fileH, array.name, array.right)
-	elif array.token == 'assign-expression':
-		fileH.write("\t{} [label=\"Assing\"]\n".format(array.name))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-		#~ print array.left,array.right
-		getItem(fileH, array.name, array.left)
-		getItem(fileH, array.name, array.right)
-	elif array.token == 'INEQUALITY-expression':
-		fileH.write("\t{} [label=\"INEQUAL:{}\"]\n".format(array.name,array.op))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-		#~ print array.left,array.right
-		getItem(fileH, array.name, array.left)
-		getItem(fileH, array.name, array.right)
-	elif array.token == 'roman-expression':
-		fileH.write("\t{} [label=\"roman:{}\"]\n".format(array.name,array.left))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-	elif array.token == 'id-expression':
-		fileH.write("\t{} [label=\"id:{}\"]\n".format(array.name,array.left))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-	elif array.token == 'group-expression':
-		fileH.write("\t{} [label=\"Group\"]\n".format(array.name))
-		if parent != "start":
-			fileH.write("\t{} -> {}\n".format(parent, array.name))
-		getItem(fileH, array.name, array.left)
-
-
+#~ exit()
+def getItem(fileH, parent, onenode, orderid = None):
+	if onenode.token == 'for-expression':
+		fileH.write("\t{} [label=\"FOR\"]\n".format(onenode.name))
+		WriteParentLine(fileH, parent, onenode, orderid)
+		for i in range(len(onenode.array)):
+			getItem(fileH, onenode.name, onenode.array[i], i+1)
+	elif onenode.token == 'binary-expression':
+		fileH.write("\t{} [label=\"Bin: {}\"]\n".format(onenode.name,onenode.op))
+		WriteParentLine(fileH, parent, onenode, orderid)
+		for i in range(len(onenode.array)):
+			getItem(fileH, onenode.name, onenode.array[i], i+1)
+	elif onenode.token == 'assign-expression':
+		fileH.write("\t{} [label=\"Assign\"]\n".format(onenode.name))
+		WriteParentLine(fileH, parent, onenode, orderid)
+		for i in range(len(onenode.array)):
+			getItem(fileH, onenode.name, onenode.array[i], i+1)
+	elif onenode.token == 'INEQUALITY-expression':
+		fileH.write("\t{} [label=\"INEQUAL: {}\"]\n".format(onenode.name,onenode.op))
+		WriteParentLine(fileH, parent, onenode, orderid)
+		for i in range(len(onenode.array)):
+			getItem(fileH, onenode.name, onenode.array[i], i+1)
+	elif onenode.token == 'ORDER-expression':
+		fileH.write("\t{} [label=\"GROUP\"]\n".format(onenode.name))
+		WriteParentLine(fileH, parent, onenode, orderid)
+		for i in range(len(onenode.array)):
+			getItem(fileH, onenode.name, onenode.array[i], i+1)
+	elif onenode.token == 'roman-expression':
+		fileH.write("\t{} [label=\"roman: {}\"]\n".format(onenode.name,onenode.array[0]))
+		WriteParentLine(fileH, parent, onenode, orderid)
+	elif onenode.token == 'id-expression':
+		fileH.write("\t{} [label=\"id: {}\"]\n".format(onenode.name,onenode.array[0]))
+		WriteParentLine(fileH, parent, onenode, orderid)
 
 
 fileH = open("graph.gv", 'w')
@@ -192,18 +192,7 @@ fileH.close()
 
 
 fileN = open("file.jpg", 'w')
-subprocess.call(["/usr/bin/dot", "-Tjpg", "/home/progga/compiler/graph.gv"],shell=False, stdout=fileN)
+subprocess.call(["/usr/bin/dot", "-Tjpg", "graph.gv"],shell=False, stdout=fileN)
 fileN.close()
-subprocess.call(["/usr/bin/gnome-open","/home/progga/compiler/file.jpg"])
+subprocess.call(["/usr/bin/gnome-open","file.jpg"])
 
-"""
-#	fileH = open("graph.gv", 'w')
-#	fileH.write("digraph G {\n")
-#	getItem(fileH, "start", res, -1)
-#	fileH.write("}\n")
-#	getItem(res,-1)
-	#~ print res
-#	t = Tree(ar)
-#	t.show()
-
-"""
